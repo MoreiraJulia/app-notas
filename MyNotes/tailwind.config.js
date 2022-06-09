b2cd550cd523eff0d2bf6f92c96c32e33d79859a
Júlia Aparecida Moreira <julia.aparecida25@gmail.com>
2022-06-09 20:46:28 -0300

    Atualizações

---
+++ MyNotes/tailwind.config.js
@@ -0,0 +1,25 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+// module.exports = {
+//   purge: [],
+//   darkMode: false, // or 'media' or 'class'
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {},
+//   plugins: [],
+// }
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
